fad908c858889318b8da27d92381dd45d880fcd4
Praveen <praveen.lowlevel.io@gmail.com>
2026-04-10 13:11:04 +0530

    Using bool

---
+++ variable6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main () {
+    bool isOnline = true;
+    printf("%d\n", isOnline); // %d shows a digit . true = 1 , false = 0;
+    bool isStudent = false;
+    printf("%d\n", isStudent);
+    
+    return isStudent; // Seems like I can also do this . cool !!
+
+}
