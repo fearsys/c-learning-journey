eec7f7f18ef14ffff5df6d333d868048ebf50bf8
Praveen <praveen.lowlevel.io@gmail.com>
2026-04-10 12:00:43 +0530

    Using variables + int data type

---
+++ variable1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main () {
+    // We will be using variables in this code
+    int age = 22;
+    int year = 2026;
+    int qnt = 5;
+
+    printf("Your age is : %d\n", age); // %d is a placeholder, d for decimal you could say
+    printf("Year is : %d\n", year);
+    printf("You have bought %d x items\n", qnt);
+
+
+}
