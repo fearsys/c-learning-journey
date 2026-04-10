f5689f9398449cd20bb9f2d6195b6ba0d914e3a6
Praveen <praveen.lowlevel.io@gmail.com>
2026-04-10 14:55:32 +0530

    Using Format Specifier

---
+++ format_specifier.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main () {
+    int age = 22;
+    float price = 19.3333;
+    double pi = 3.141592653589793;
+    char name[] = "Praveen Kumar";
+    char grade = 'A';
+
+    printf("Age:     %d\n", age);
+    printf("Price:   %f\n", price);
+    printf("Pi:      %f\n", pi);
+    printf("Name:    %s\n", name);
+    printf("Grade:   %c\n", grade);
+
+    return 0;
+}
