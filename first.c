8fdb50d3ddc5e9bc5a35649ca5728599004fe2a2
Praveen <praveen.lowlevel.io@gmail.com>
2026-04-10 11:37:28 +0530

    First program with comments

---
+++ first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    
+    // This is my first comment. Comment will not be displayed in the output.
+
+    /* This is a long comment or you can say 
+    a multi line comment . this won't be displayed either */
+
+    printf("This is my first C program\n");
+    printf("I am excited to learn c \n");
+    return 0;
+}
