68194ec9e3487683b42a32301270b4560224d51d
Praveen <praveen.lowlevel.io@gmail.com>
2026-04-11 13:38:16 +0530

    Using maths functions to build circle

---
+++ circle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+
+    double radius = 0.0;
+    double area = 0.0;
+    const double PI = 3.14159;
+    double circumference = 0.0;
+
+    printf("Radius (in cms): ");
+    scanf("%lf", &radius);
+
+    area = PI * radius * pow(radius, 2);
+    circumference = 2 * PI * radius;
+
+    printf("Area: %f cms sq.\n", area);
+    printf("Circumference: %f cms\n", circumference);
+
+    return 0;
+}
