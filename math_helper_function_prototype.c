f49484e21a2c304fe81eda94e1191c810b55d062
Praveen <praveen.lowlevel.io@gmail.com>
2026-04-12 17:09:10 +0530

    Using function prototype in my math helper project

---
+++ math_helper_function_prototype.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+// This is same as my previous programs the only difference is I am using function prototype here. 
+// The main advantage is you can just give a reference to the function you are going to use later on to the compiler and also to the person reading . and then you can work on your main function and then after the main function has ended we can write our other function 
+void printHeader(float a, float b) ; // Every prototype needs a semicolon at the end
+float add(float a, float b) ;
+float subtract(float a, float b) ;
+float multiply(float a, float b) ;
+float divide(float a, float b) ;
+
+int main () {
+
+    float a = 0.0f;
+    float b = 0.0f;
+
+    printf("Number 1: ");
+    scanf("%f", &a);
+    printf("Number 2: ");
+    scanf("%f", &b);
+
+    printHeader(a, b);
+    printf("Add:        %.2f\n", add(a,b));
+    printf("Subtract:   %.2f\n", subtract(a,b));
+    printf("Multiply:   %.2f\n", multiply(a,b));
+    float result = divide(a, b);
+    if (result == 0) {
+        printf("Division by zero error\n");
+    } 
+    else {
+        printf("Divide:     %.2f\n", result);
+    }
+    printf("==============================\n");
+
+    return 0;
+
+}
+
+void printHeader(float a, float b) {
+     printf("==============================\nMATH HELPER RESULTS\n==============================\nNumber 1:   %.2f\nNumber 2:   %.2f\n------------------------------\n", a, b);
+}
+
+float add(float a, float b) {
+    float result = a + b;
+    return result;
+}
+
+float subtract(float a, float b) {
+    float result = a - b;
+    return result;
+}
+
+float multiply(float a, float b) {
+    float result = a * b;
+    return result;
+}
+
+float divide(float a, float b) {
+    if (b == 0) {
+        return 0;
+    }
+    return a / b;
+}
