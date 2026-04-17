b877904eae3918edce2400a914b5b62b0ba4e400
Praveen <praveen.lowlevel.io@gmail.com>
2026-04-17 21:42:56 +0530

    contact book v4: Added a search feature

--- contact_book.c
+++ contact_book.c
@@ -14,16 +14,18 @@ void userInput(Contact contacts[], int size);
 void headerPrinter();
 void contactPrinter(Contact contacts[], int size);
 void columnPrinter();
+void margin();
+void searchContact(Contact contacts[], int size);
 
 int main () {
-
-    // Store up to 5 contacts via user input
 
     Contact contacts[CONTACT_SIZE] = {0};
     userInput(contacts, CONTACT_SIZE);
     headerPrinter();
     columnPrinter();
     contactPrinter(contacts, CONTACT_SIZE);
+    margin();
+    searchContact(contacts,CONTACT_SIZE);
 
     return 0;
 }
@@ -63,4 +65,24 @@ void contactPrinter(Contact contacts[], int size) {
 
 void columnPrinter() {
     printf("SL.            NAME                   PHONE                EMAIL\n");
+}
+
+void margin() {
+    printf("====================================================\n"); 
+}
+
+void searchContact(Contact contacts[], int size) {
+    char query[30];
+    printf("Enter name to search: ");
+    fgets(query, sizeof(query), stdin);
+    removeNewline(query);
+
+    for (int i = 0; i < size; i++) {
+        if (strcmp(contacts[i].name, query) == 0) {
+            printf("Found!\n");
+            printf("%-30s %-15s %-30s\n",contacts[i].name, contacts[i].phone,contacts[i].email);
+            return;
+        }
+    }
+    printf("Contact not found.\n");
 }
