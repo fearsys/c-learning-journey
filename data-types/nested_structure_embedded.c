c8684b4101ff6a74d2284d6d90f2020cddb246c7
Praveen <praveen.lowlevel.io@gmail.com>
2026-04-28 20:43:25 +0530

    Using embedded nested strucutres in c

---
+++ data-types/nested_structure_embedded.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+struct School {
+    char name[30];
+    int schoolId;
+    struct Student {
+        char name[30];
+        int age;
+        int marks;
+    }st;
+};
+
+int main () {
+
+    struct School dav = {"DAV", 101, "Jai", 21, 93};
+
+    printf("%s %d %s %d %d\n", dav.name, dav.schoolId, dav.st.name, dav.st.age, dav.st.marks);
+
+    struct Student pk = {"Praveen", 22, 78};
+
+    printf("%s %d %d\n", pk.name, pk.age, pk.marks);
+
+    return 0;
+
+}
