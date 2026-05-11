c7ce0038d3429564292bef291804f7df6cc94079
Praveen <praveen.lowlevel.io@gmail.com>
2026-05-11 15:42:21 +0530

    Using recursion to find base raised to the power of some n numbers

---
+++ Functions/recursion2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int power(int base, int exp);
+
+int main () {
+
+    printf("Result: %d\n", power(2,5));
+    return 0;
+}
+
+int power(int base, int exp) {
+    if (exp <= 0) return 1;
+    return base * power(base, exp-1);
+}
