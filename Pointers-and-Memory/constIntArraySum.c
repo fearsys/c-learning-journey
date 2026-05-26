cf39fa0a0cfd343ccaf1c1d597851fcb339fb0d0
Praveen <praveen.lowlevel.io@gmail.com>
2026-05-26 13:51:25 +0530

    A safer version of sum array using const int

---
+++ Pointers-and-Memory/constIntArraySum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int sumArray(const int *arr, int size);
+
+int main() {
+    int nums[] = {5, 12, 7, 20, 9};
+    int size = sizeof(nums) / sizeof(nums[0]);
+
+    int sum = sumArray(nums, size);
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
+
+int sumArray(const int *arr, int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
