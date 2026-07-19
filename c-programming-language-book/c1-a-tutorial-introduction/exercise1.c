5c3d71a9223b5cb53f2135f786e810dc09983f98
Praveen <praveen.lowlevel.io@gmail.com>
2026-07-19 14:05:25 +0530

    Some basic programs in C

---
+++ c-programming-language-book/c1-a-tutorial-introduction/exercise1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main () {
+    printf("Hello, World\n"); //Needs a return type int 
+    return 0;
+}
---
+++ c-programming-language-book/c1-a-tutorial-introduction/exercise2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main () {
+    printf("Hello,\c World\n"); // c is an unknown escape sequence so the code does not work 
+    return 0;
+}
